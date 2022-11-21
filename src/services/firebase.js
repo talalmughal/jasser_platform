6d64cf047682b22040831ab6949a181d160953be
Talal Nasir <talalmughal47@gmail.com>
2022-11-22 00:03:56 +0500

    configured firebase firestore

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-BbT_KyQKSac1k7Awb9xqvkY3IyNdqTc",
+  authDomain: "jasser-e977f.firebaseapp.com",
+  projectId: "jasser-e977f",
+  storageBucket: "jasser-e977f.appspot.com",
+  messagingSenderId: "559505305076",
+  appId: "1:559505305076:web:eadbd93f90d37275281da1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
