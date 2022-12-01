fc3996a1dc329bc9170000609b86084d7031dce1
Talal Nasir <talalmughal47@gmail.com>
2022-12-02 00:07:11 +0500

    Added signup functionality: applicant and employer created, files storing in fb storage, error handling

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+} from "firebase/auth";
+
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,8 +15,76 @@ const firebaseConfig = {
   projectId: "jasser-e977f",
   storageBucket: "jasser-e977f.appspot.com",
   messagingSenderId: "559505305076",
-  appId: "1:559505305076:web:eadbd93f90d37275281da1"
+  appId: "1:559505305076:web:eadbd93f90d37275281da1",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+export const db = getFirestore(app);
+export const auth = getAuth();
+
+//  Initialize firebase-storage
+export const storage = getStorage(app);
+
+// for user to sign up
+export async function signup(email, password) {
+  try {
+    const ret = await createUserWithEmailAndPassword(auth, email, password);
+    return ret;
+  } catch (error) {
+    console.log("Something went wrong in firebase/signup funtion: ", error);
+    return error;
+  }
+}
+
+// to create an applicant
+export async function createApplicant(userData, applicantDocRef) {
+  try {
+    await setDoc(doc(db, "applicant", applicantDocRef), userData);
+  } catch (error) {
+    console.log(
+      "Something went wrong in firebase/createApplicant funtion: ",
+      error
+    );
+  }
+}
+
+// to create an employer
+export async function createEmployer(userData, employerDocRef) {
+  try {
+    await setDoc(doc(db, "employer", employerDocRef), userData);
+  } catch (error) {
+    console.log(
+      "Something went wrong in firebase/createEmployer funtion: ",
+      error
+    );
+  }
+}
+
+// to upload profile-picture to firebase storage
+export async function uploadProfilePicture(profilePicture, storageFolder) {
+  try {
+    const imageRef = ref(storage, `${storageFolder}/ProfilePicture`);
+    return uploadBytes(imageRef, profilePicture);
+  } catch (error) {
+    console.log(
+      "Something went wrong in firebase/uploadProfilePicture function: ",
+      error
+    );
+  }
+}
+
+// to upload resume to firebase storage
+export async function uploadResume(resume, storageFolder) {
+  try {
+    const resumeRef = ref(storage, `${storageFolder}/Resume`);
+    return uploadBytes(resumeRef, resume);
+  } catch (error) {
+    console.log(
+      "Something went wrong in firebase/uploadResume function: ",
+      error
+    );
+  }
+}
